a13b8f5f8767c5f662e930664328259e089953d5
alngrc <alan.garciag10@outlook.com>
2023-04-17 12:53:44 -0300

    Cambios

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
-// var meta = JSON.parse('https://epic.gsfc.nasa.gov/api/natural');
+window.addEventListener('load', obtenerDatos);
 
-// $.ajax('https://epic.gsfc.nasa.gov/api/natural', {
-//         success : function(iDataArr, stat, xhr) {
-//             // do something with the list
-//         }
-//     });
-$(document).ready(function (){
-    $.ajax({
-        method: "GET",
-        url: "https://epic.gsfc.nasa.gov/api/natural",
-    }).done(function (respuestaImg){
-        
-    })
-}) 
+function obtenerDatos(){
+    const Nasa_api = 'vZQVBQWVQr1EmRgO2Avwlnjrau0SaX9szdgjnI8T';
+    const ruta = `https://api.nasa.gov/planetary/apod?api_key=${Nasa_api}`;
+
+    fetch(ruta)
+    .then(respuesta => respuesta.json())
+    .then(resultado => mostrarDatos(resultado))
+}
+function mostrarDatos({date, title, media_type, url, copyright}) {
+    const titulo = document.querySelector('#titulo-apod');
+    titulo.innerHTML = title;
+    const fecha = document.querySelector('#fecha-apod');
+    fecha.innerHTML = date;
+    const copy = document.querySelector('#copy-apod');
+    copy.innerHTML = copyright;
+    
+
+    const multimedia = document.querySelector('#c_multimedia');
+    if (media_type == 'video') {
+        multimedia.innerHTML = `<iframe class="embed-responsive-item" src="${url}"></iframe>`
+
+    } else {
+        multimedia.innerHTML = `<img src="${url}" class="img-fluid" alt="${url}">`
+    }
+
+}
+
+// $(document).ready(function (){
+//     $.ajax({
+//         method: "GET",
+//         url: "https://api.nasa.gov/planetary/apod" ,
+//         // url: "https://api.nasa.gov/planetary/apod?api_key=vZQVBQWVQr1EmRgO2Avwlnjrau0SaX9szdgjnI8T",
+//     }).done(function (respuestaImg){
+//         path_img
+//     })
+// }) 
